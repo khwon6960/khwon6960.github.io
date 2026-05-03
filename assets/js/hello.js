4c4d270d79f9345141779ef6286c90b12b9b32ef
khwon6960 <khwon6960@snu.ac.kr>
2026-05-03 08:19:27 +0000

    과제 9

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,12 +1,9 @@
 // assets/js/hello.js
-const author = "이상";
-const title = "날개";
-const year = 1936;
-const today = 2026;
-const yearsPassed = today - year;
+const name = "김효원";
+const major = "언어학";
+const openYear = 2026;
 
-console.log(
-    `안녕하세요! ${author}의 「${title}」 (${year}) ` +
-    `페이지에 오신 것을 환영합니다.`
-);
-console.log(`이 작품은 발표된 지 ${yearsPassed}년이 되었습니다.`);
+const greeting = `안녕하세요! ${name}(${major} 전공)의 사이트입니다.
+이 사이트는 ${openYear})년에 개설되었습니다.`;
+
+console.log(greeting);
