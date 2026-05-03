72f6dd0313cb672063d60c72ffc34c9aabd60763
khwon6960 <khwon6960@snu.ac.kr>
2026-05-03 08:27:44 +0000

    hello.js name을 writer로 수정

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,9 +1,9 @@
 // assets/js/hello.js
-const name = "김효원";
+const writer = "김효원";
 const major = "언어학";
 const openYear = 2026;
 
-const greeting = `안녕하세요! ${name}(${major} 전공)의 사이트입니다.
+const greeting = `안녕하세요! ${writer}(${major} 전공)의 사이트입니다.
 이 사이트는 ${openYear})년에 개설되었습니다.`;
 
 console.log(greeting);
